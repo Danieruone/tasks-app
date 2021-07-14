da43024476242e03c292d02ac3a6966789ea4527
Daniel <danieruone@gmail.com>
2021-07-14 16:59:57 -0500

    constructor and ondestroy example

--- src/app/task-card/task-card.component.ts
+++ src/app/task-card/task-card.component.ts
@@ -1,10 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
   styleUrls: ['./task-card.component.css']
 })
-export class TaskCardComponent {
+export class TaskCardComponent implements OnDestroy {
+  constructor(){
+    alert("tarjeta creada, este es el constructor")
+  }
+
   @Input() nombre:string = "Default"
+  @Input() tareas:string[] = []
+
+  eliminarTarea(){
+    this.tareas.splice(0, 1);
+  }
+  
+  ngOnDestroy(){
+    alert("tarjeta destruida, este es el ondestroy")
+  }
 }
