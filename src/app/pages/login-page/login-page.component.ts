716e5d9d162ad6bdb2d59197beae5a2e548076cc
Daniel <danieruone@gmail.com>
2021-08-08 17:28:22 -0500

    login service

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RestService } from 'src/app/rest.service';
+import {Router} from "@angular/router"
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
+  email!: string;
+  password!: string;
 
-  constructor() { }
+  constructor(private restService: RestService, private router: Router) {}
 
-  ngOnInit(): void {
+  OnInputEmail(event: any) {
+    this.email = event.target.value;
   }
 
+  OnInputPassword(event: any) {
+    this.password = event.target.value;
+  }
+
+  loginWithData(e: any) {
+    e.preventDefault();
+    const payload = {
+      email: this.email,
+      password: this.password,
+    };
+    this.restService.login(payload).subscribe(
+      // to do: save token and user id
+      (data) => this.router.navigate(['/tasks']),
+      (error) => {
+        Swal.fire('Error', error, 'error');
+      }
+    );
+  }
 }
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RestService {
+  base_url = 'http://localhost:8080/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public login(payload: any) {
+    return this.http
+      .post(this.base_url + 'login', payload)
+      .pipe(catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    return throwError(error.error.err.message || 'Server error');
+  }
 }
